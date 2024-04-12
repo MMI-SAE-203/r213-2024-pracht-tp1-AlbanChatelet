290ee39dbadc95580dc58a269402120ef969edd9
AlbanChatelet <alban.chatelet@edu.univ-fcomte.fr>
2024-04-12 13:38:14 +0200

    ajout du menu en html

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts">
-//
+import { ref } from 'vue';
+
+const maVar = ref(true)
 </script>
 
 <template>
-  <h1 class="text-2xl">Bonjour monde !</h1>
+  
+  <button @click="maVar = false">Cacher</button>
+  <button @click="maVar = true">Montrer</button>
+  <button @click="maVar = !maVar"> Bascule</button>
+  
+  <h1 v-show="maVar"
+  class="text-2xl">Quand j'arrive au lundi</h1>
 </template>
