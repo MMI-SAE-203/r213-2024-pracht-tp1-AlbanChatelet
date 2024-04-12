dccf8bb68cf04a4edaf4d7a1a78431a47f3eb0e8
AlbanChatelet <alban.chatelet@edu.univ-fcomte.fr>
2024-04-12 14:31:37 +0200

    ajout du code avec v for

--- src/pages/donnees.vue
+++ src/pages/donnees.vue
@@ -2,36 +2,38 @@
   import { ref } from 'vue';
 
   const sectionsData = [
-  {
-    label: 'bouton 1',
-    texte: `texte panneau 1 Lorem ipsum dolor sit ame
-    consectetur adipisicing elit. Inventore animi dolore,
-    rerum magni laudantium quod excepturi laboriosam, 
-    ex modi debitis harum reprehenderit eaque quam ut
-    ea molestiae. Id, cum dolor!`
-  },
-  {
-    label: 'bouton 2',
-    texte: `texte panneau 2 Amet alias provident quos quis, 
-    Officia ut ab dolores quos dolorem accusamus ad,
-    consectetur unde minima, ipsum eligendi inventore id
-    labore, laborum rerum laboriosam corrupti iste.
-    Distinctio, perspiciatis!`
-  },
-  {
-    label: 'bouton 3',
-    texte: `texte panneau 3 Repudiandae corporis voluptates, 
-    odit reprehenderit sint pariatur at voluptatum, cumque
-    quia sit eligendi ex culpa eos, alias magnam molestiae
-    id modi accusantium ipsa eveniet accusamus. Tempora 
-    quis corporis et nam.`
-  }
-]
-
+    {
+      label: 'bouton 1',
+      texte: `texte panneau 1 Lorem ipsum dolor sit ame
+      consectetur adipisicing elit. Inventore animi dolore,
+      rerum magni laudantium quod excepturi laboriosam, 
+      ex modi debitis harum reprehenderit eaque quam ut
+      ea molestiae. Id, cum dolor!`
+    },
+    {
+      label: 'bouton 2',
+      texte: `texte panneau 2 Amet alias provident quos quis, 
+      Officia ut ab dolores quos dolorem accusamus ad,
+      consectetur unde minima, ipsum eligendi inventore id
+      labore, laborum rerum laboriosam corrupti iste.
+      Distinctio, perspiciatis!`
+    },
+    {
+      label: 'bouton 3',
+      texte: `texte panneau 3 Repudiandae corporis voluptates, 
+      odit reprehenderit sint pariatur at voluptatum, cumque
+      quia sit eligendi ex culpa eos, alias magnam molestiae
+      id modi accusantium ipsa eveniet accusamus. Tempora 
+      quis corporis et nam.`
+    }
+  ]
 </script>
 
 <template>
-    <h1>Données</h1>
-
-
+  <h1>Données</h1>
+  <section v-for="(section, key) of sectionsData" :key="key">
+    <pre class="font-mono">key : {{ key }}</pre>
+    <pre class="font-mono">label : {{ section.label }}</pre>
+    <pre class="font-mono">texte : {{ section.texte }}</pre>
+  </section>
 </template>
