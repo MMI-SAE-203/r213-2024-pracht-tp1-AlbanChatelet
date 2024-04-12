a46b2d4b81cf288d83a7cbb6f80e2b010d12e3a5
AlbanChatelet <alban.chatelet@edu.univ-fcomte.fr>
2024-04-12 13:53:13 +0200

    menu fonctionnel avec v-show

--- src/App.vue
+++ src/App.vue
@@ -11,21 +11,23 @@ const menuIsOpen = ref(false)
 
 <template>
   <header>
-                  <button
-                aria-controls="mainNav"
-                aria-expanded="true"
-                class="rounded-full border-2 border-red-600 bg-red-300 px-2"
-              >
-                menu
-              </button>
-              
-              <nav id="mainNav">
-                <ul>
-                  <li><a href="#">item 1</a></li>
-                  <li><a href="#">item 2</a></li>
-                  <li><a href="#">item 3</a></li>
-                </ul>
-              </nav>
+    <button
+      aria-controls="mainNav"
+      aria-expanded="true"
+      class="rounded-full border-2 border-red-600 bg-red-300 px-2"
+      @click="menuIsOpen = !menuIsOpen"
+    >
+      menu
+    </button>
+    
+    <nav id="mainNav" v-show="menuIsOpen">
+      <ul>
+        <li><a href="#">item 1</a></li>
+        <li><a href="#">item 2</a></li>
+        <li><a href="#">item 3</a></li>
+      </ul>
+    </nav>
+
     <nav>
       <ul>
         <li>
